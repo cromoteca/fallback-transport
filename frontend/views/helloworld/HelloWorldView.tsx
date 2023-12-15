11594405ad1de61f152ba56e5247df97eb789283
Luciano Vernaschi <luciano@vaadin.com>
2023-12-15 10:53:49 +0100

    WIP

--- frontend/views/helloworld/HelloWorldView.tsx
+++ frontend/views/helloworld/HelloWorldView.tsx
@@ -1,30 +1,17 @@
-import { Button } from '@hilla/react-components/Button.js';
-import { Notification } from '@hilla/react-components/Notification.js';
-import { TextField } from '@hilla/react-components/TextField.js';
 import { HelloWorldService } from 'Frontend/generated/endpoints.js';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function HelloWorldView() {
-  const [name, setName] = useState('');
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const subscription = HelloWorldService.countForever()
+    subscription.onNext(setCount);
+
+    return () => subscription.cancel();
+  }, []);
 
   return (
-    <>
-      <section className="flex p-m gap-m items-end">
-        <TextField
-          label="Your name"
-          onValueChanged={(e) => {
-            setName(e.detail.value);
-          }}
-        />
-        <Button
-          onClick={async () => {
-            const serverResponse = await HelloWorldService.sayHello(name);
-            Notification.show(serverResponse);
-          }}
-        >
-          Say hello
-        </Button>
-      </section>
-    </>
-  );
+    <div className='p-m'>{count}</div>
+  )
 }
--- src/main/java/com/example/application/services/HelloWorldService.java
+++ src/main/java/com/example/application/services/HelloWorldService.java
@@ -1,8 +1,17 @@
 package com.example.application.services;
 
+import java.time.Duration;
+import java.util.logging.Level;
+
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+
 import dev.hilla.BrowserCallable;
+
 import org.springframework.stereotype.Service;
+
+import dev.hilla.EndpointSubscription;
+import reactor.core.publisher.Flux;
+import reactor.util.retry.Retry;
 
 @BrowserCallable
 @AnonymousAllowed
@@ -15,5 +24,13 @@ public class HelloWorldService {
         } else {
             return "Hello " + name;
         }
+    }
+
+    public Flux<Long> countForever() {
+        return Flux.interval(Duration.ofMillis(400))
+                .onBackpressureBuffer()
+                .doOnNext(tick -> System.out.println("Tick: " + tick))
+                .doOnError(err -> System.out.println("Error: " + err))
+                .retryWhen(Retry.indefinitely().doAfterRetry(rs -> System.out.println("Retry: " + rs)));
     }
 }
